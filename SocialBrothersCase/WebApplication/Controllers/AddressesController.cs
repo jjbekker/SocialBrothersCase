d5172dd990c5137955de5505be644f1a2d7286d2
jjbekker <jj.bekker@student.avans.nl>
2020-05-07 21:13:31 +0200

    added db and webappcontroller

--- SocialBrothersCase/WebApplication/Controllers/AddressesController.cs
+++ SocialBrothersCase/WebApplication/Controllers/AddressesController.cs
@@ -7,40 +7,68 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using API.Database;
 using Domain.Model;
+using WebApplication.Helpers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace WebApplication.Controllers
 {
     public class AddressesController : Controller
     {
-        private readonly DBInteractor _context;
-
-        public AddressesController(DBInteractor context)
+        private ApiHelper _api;
+        public AddressesController()
         {
-            _context = context;
+            _api = new ApiHelper();
         }
 
         // GET: Addresses
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString, string sort)
         {
-            return View(await _context.Address.ToListAsync());
+            List<Address> adresses = new List<Address>();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("/api/Addresses");
+
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                adresses = JsonConvert.DeserializeObject<List<Address>>(result);
+            }
+            if (!String.IsNullOrEmpty(searchString) && String.IsNullOrEmpty(sort))
+            {
+                adresses = adresses.Where(s => s.Street.Contains(searchString)
+                || s.Country.Contains(searchString)
+                || s.City.Contains(searchString)
+                || s.PostalCode.Contains(searchString)
+                || s.HouseNumber.ToString().Contains(searchString))
+                    .ToList();
+            }
+            else if (!String.IsNullOrEmpty(sort))
+            {
+                adresses = adresses.OrderBy(s => s.Street).ToList();
+            }
+            return View(adresses);
         }
 
         // GET: Addresses/Details/5
         public async Task<IActionResult> Details(long? id)
         {
-            if (id == null)
+            Address addresses = new Address();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("/api/Addresses/" + id);
+
+            if (res.IsSuccessStatusCode)
+            {
+                var result = res.Content.ReadAsStringAsync().Result;
+                addresses = JsonConvert.DeserializeObject<Address>(result);
+            }
+
+            if (addresses == null)
             {
                 return NotFound();
             }
 
-            var address = await _context.Address
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-
-            return View(address);
+            return View(addresses);
         }
 
         // GET: Addresses/Create
@@ -56,28 +84,38 @@ namespace WebApplication.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Street,HouseNumber,PostalCode,City,Country")] Address address)
         {
-            if (ModelState.IsValid)
+            Address newAddress = new Address();
+            using (var httpClient = _api.Initial())
             {
-                _context.Add(address);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                StringContent content = new StringContent(JsonConvert.SerializeObject(address), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync("/api/Addresses/", content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    newAddress = JsonConvert.DeserializeObject<Address>(apiResponse);
+                }
             }
-            return View(address);
+            return RedirectToAction("Index");
         }
 
         // GET: Addresses/Edit/5
         public async Task<IActionResult> Edit(long? id)
         {
-            if (id == null)
+            Address address = new Address();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("/api/Addresses/" + id);
+
+            if (res.IsSuccessStatusCode)
             {
-                return NotFound();
+                var result = res.Content.ReadAsStringAsync().Result;
+                address = JsonConvert.DeserializeObject<Address>(result);
             }
 
-            var address = await _context.Address.FindAsync(id);
             if (address == null)
             {
                 return NotFound();
             }
+
             return View(address);
         }
 
@@ -88,44 +126,41 @@ namespace WebApplication.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(long id, [Bind("Id,Street,HouseNumber,PostalCode,City,Country")] Address address)
         {
-            if (id != address.Id)
+            Address newAddress = new Address();
+            using (var httpClient = _api.Initial())
             {
-                return NotFound();
-            }
+                var content = new MultipartFormDataContent();
+                content.Add(new StringContent(address.Id.ToString()), "Id");
+                content.Add(new StringContent(address.Street), "Street");
+                content.Add(new StringContent(address.HouseNumber.ToString()), "HouseNumber");
+                content.Add(new StringContent(address.PostalCode), "PostalCode");
+                content.Add(new StringContent(address.City), "City");
+                content.Add(new StringContent(address.Country), "Country");
+                var json = JsonConvert.SerializeObject(content);
 
-            if (ModelState.IsValid)
-            {
-                try
+                using (var response = await httpClient.PutAsync("/api/Addresses/" + address.Id, content))
                 {
-                    _context.Update(address);
-                    await _context.SaveChangesAsync();
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Result = "Success";
+                    address = JsonConvert.DeserializeObject<Address>(apiResponse);
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AddressExists(address.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
             }
-            return View(address);
+            return RedirectToAction("Index");
         }
 
         // GET: Addresses/Delete/5
         public async Task<IActionResult> Delete(long? id)
         {
-            if (id == null)
+            Address address = new Address();
+            HttpClient client = _api.Initial();
+            HttpResponseMessage res = await client.GetAsync("/api/Addresses/" + id);
+
+            if (res.IsSuccessStatusCode)
             {
-                return NotFound();
+                var result = res.Content.ReadAsStringAsync().Result;
+                address = JsonConvert.DeserializeObject<Address>(result);
             }
 
-            var address = await _context.Address
-                .FirstOrDefaultAsync(m => m.Id == id);
             if (address == null)
             {
                 return NotFound();
@@ -135,19 +170,19 @@ namespace WebApplication.Controllers
         }
 
         // POST: Addresses/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
-            var address = await _context.Address.FindAsync(id);
-            _context.Address.Remove(address);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+            using (var httpClient = _api.Initial())
+            {
+                using (var response = await httpClient.DeleteAsync("api/Addresses/" + id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                }
+            }
 
-        private bool AddressExists(long id)
-        {
-            return _context.Address.Any(e => e.Id == id);
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ SocialBrothersCase/WebApplication/Helpers/ApiHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApplication.Helpers
+{
+    public class ApiHelper
+    {
+        public HttpClient Initial()
+        {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:61224");
+            return client;
+        }
+    }
+}
